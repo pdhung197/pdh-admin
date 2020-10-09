edcb0054bf21da30ff4cd5a4e14959f1cf0de4dc
King Phan <hphan@openwt.com>
2020-10-09 23:18:23 +0700

    Updates

---
+++ precache-manifest.7da26ac745aefe0ba000d4ab1f3cf54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ab796594a1035d03e53fb711045d3f",
+    "url": "/pdh-admin/index.html"
+  },
+  {
+    "revision": "8269d09a1bca0ee95fc2",
+    "url": "/pdh-admin/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "8269d09a1bca0ee95fc2",
+    "url": "/pdh-admin/static/js/2.cd21a736.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pdh-admin/static/js/2.cd21a736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b282d3430220c818c11",
+    "url": "/pdh-admin/static/js/main.189f7edd.chunk.js"
+  },
+  {
+    "revision": "7a7f402e94e02d485649",
+    "url": "/pdh-admin/static/js/runtime-main.600a2eb9.js"
+  }
+]);
